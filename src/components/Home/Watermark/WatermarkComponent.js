8883d5fd47f99505ade7e33ac0f977f469a73062
Jordan Barbosa <jordansbarbosa01@gmail.com>
2019-11-21 00:25:06 -0500

    Eddited class to className

--- src/components/Home/Watermark/WatermarkComponent.js
+++ src/components/Home/Watermark/WatermarkComponent.js
@@ -6,7 +6,7 @@ export default class WatermarkComponent extends PureComponent {
     render() {
         return (
             <div className="watermark-container">
-                <h1>Made by <a target="_blank" rel="noopener noreferrer" href="https://jordanbarbosa.com">Jordan Barbosa</a> <a href="https://github.com/jordanjordanb1/simple-weather-app" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a></h1>
+                <h1>Made by <a target="_blank" rel="noopener noreferrer" href="https://jordanbarbosa.com">Jordan Barbosa</a> <a href="https://github.com/jordanjordanb1/simple-weather-app" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a></h1>
             </div>
         )
     }
