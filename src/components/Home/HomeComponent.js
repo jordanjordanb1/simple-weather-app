1b10343b6a1fc77594529822120bc5c2b0c83cae
Jordan Barbosa <jordansbarbosa01@gmail.com>
2019-11-21 00:24:49 -0500

    Moved components around

--- src/components/Home/HomeComponent.js
+++ src/components/Home/HomeComponent.js
@@ -4,13 +4,17 @@ import { Container } from 'reactstrap'
 import './HomeComponent.css'
 
 import Watermark from './Watermark/WatermarkComponent'
+import SearchBarComponent from './SearchBar/SearchBarComponent'
 
 export default class HomeComponent extends PureComponent {
     render() {
         return (
-            <Container className="weather-container" fluid={true}>
+            <>
+                <Container className="weather-container" fluid={true}>
+                    <SearchBarComponent />
+                </Container>
                 <Watermark />
-            </Container>
+            </>
         )
     }
 }
