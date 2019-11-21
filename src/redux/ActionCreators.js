9b4f625216f158a8419eae7665757cff3ceb1961
Jordan Barbosa <jordansbarbosa01@gmail.com>
2019-11-21 00:24:32 -0500

    Added a fetchweather to use with openweather api

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,1 +1,9 @@
-import * as ActionTypes from './ActionTypes'
+// import * as ActionTypes from './ActionTypes'
+import axios from 'axios'
+
+export const fetchWeather = value => dispatch => {
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${value},US&appid=4feebab3b716b411881417ad9a91ee0f`)
+         .then(response => {
+            return console.log(response)
+         })
+}
