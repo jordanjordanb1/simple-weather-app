8166210a00a9c14104592e749d4c00fa02a8593e
Jordan Barbosa <jordansbarbosa01@gmail.com>
2019-11-21 20:59:22 -0500

    .

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -2,7 +2,9 @@ import * as ActionTypes from './ActionTypes'
 import axios from 'axios'
 
 export const fetchWeather = value => dispatch => {
-    axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${value},US&appid=4feebab3b716b411881417ad9a91ee0f`)
+    const weatherAPI = process.env.WEATHER_API
+
+    axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${value},US&appid=${weatherAPI}`)
          .then(response => {
             return dispatch({
                 type: ActionTypes.INSERT_DATA,
