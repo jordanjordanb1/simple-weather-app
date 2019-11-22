28ff8d94314c3ee98ac1a9b881354357974cd28b
Jordan Barbosa <jordansbarbosa01@gmail.com>
2019-11-21 20:53:46 -0500

    Added a dispatch to insert response data into redux store

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,9 +1,12 @@
-// import * as ActionTypes from './ActionTypes'
+import * as ActionTypes from './ActionTypes'
 import axios from 'axios'
 
 export const fetchWeather = value => dispatch => {
-    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${value},US&appid=4feebab3b716b411881417ad9a91ee0f`)
+    axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${value},US&appid=4feebab3b716b411881417ad9a91ee0f`)
          .then(response => {
-            return console.log(response)
+            return dispatch({
+                type: ActionTypes.INSERT_DATA,
+                payload: response.data
+            })
          })
 }
